ba0ebb0d7a96ac2b05902a1b2ad402cbfc2ce915
Deligent2 <148380705+Deligent2@users.noreply.github.com>
2023-12-18 18:08:09 +0300

    finished

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
 import {Link} from "react-router-dom";
-// import Logo from '../image/logo2.png'
+import Logo from '../Image/Good-food-logo-design-on-transparent-background-PNG (2).png'
 
 
 const Header = ()=>{
     return(
         <header>
-            {/* <a href="/"> <img src={Logo} alt=""/></a> */}
+            <a href="/"> <img className={'Logo'} src={Logo} alt=""/></a>
          <nav>
              <Link to={'/'} className={'link_title'}>Главное</Link>
              <Link to={'/search'} className={'link_title'}>Найти блюдо</Link>
